f34efeda30ee012cc9d4015cf8bc0f40fe4d0c54
hnlarsen <40275454+hnlarsen@users.noreply.github.com>
2018-10-03 13:12:08 -0600

    Add files via upload

--- DFA.java
+++ DFA.java
@@ -2,39 +2,50 @@ package fa.dfa;
 
 import fa.State;
 import java.util.Set;
-import java.util.AbstractSet;
 import java.util.Map;
-import java.util.concurrent.ConcurrentMap;
-
+import java.util.HashSet;
+import java.util.HashMap;
 /**
- *  
+ *  Class for creating a DFA object containing specified states using a specified
+ *  alphabet.
  *  @author Heather N. Larsen, Taylor Roberts
  *  @version    1.0     2018/09/14:13:52
  */
 public class DFA implements fa.FAInterface, fa.dfa.DFAInterface {
-    AbstractSet Q;
-    ConcurrentMap d;
+    /** A set of states within the DFA */
+    private Set<DFAState> Q;
+    /** The alphabet for the language of the DFA */
+    private Set sigma;
+    /** Map for DFA transitions and states */
+    private HashMap map;
+    /** DFA start state */
+    private DFAState q0;
+    /***************************GLOBAL.VARIABLES********************************/
     /**
      *  Default constructor.
      */
     public DFA() {
-        
+        Q       = new HashSet<DFAState>();
+        sigma   = new HashSet();
+        map     = new HashMap();
+        q0      = null;
     }/*********************************DFA**************************************/
-
     @Override
     public void addStartState(String name) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
+        q0 = new DFAState(name);
+        Q.add(q0);
+    }/*******************************ADD.START.STATE****************************/
     @Override
     public void addState(String name) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
+        DFAState newState = new DFAState(name);
+        Q.add(newState);
+    }/********************************ADD.STATE*********************************/
     @Override
     public void addFinalState(String name) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        DFAState finalState = new DFAState(name);
+        finalState.setFinal();
+        Q.add(finalState);
+    }/******************************ADD.FINAL.STATE*****************************/
 
     @Override
     public void addTransition(String fromState, char onSymb, String toState) {
@@ -73,6 +84,14 @@ public class DFA implements fa.FAInterface, fa.dfa.DFAInterface {
     
     @Override
     public String toString() {
-        
+        String s = "Q = { " /*+ states*/ + " }" + System.lineSeparator()
+                + "Sigma = { " /*+ alphabet*/ + " }" + System.lineSeparator()
+                + "Delta =" + System.lineSeparator()
+                + "\t\t" /*+alphabet sep by \t*/ + System.lineSeparator()
+                + "\t" /*+states sep by \t acc to alphabet*/ + System.lineSeparator()
+                + "q0 = " /*+start state*/ + System.lineSeparator()
+                + "F = { " /*+final states*/ + " }" + System.lineSeparator();
+                
+        return s;
     }
 }/***********************************DFA_CLASS**********************************/
--- DFAState.java
+++ DFAState.java
@@ -1,14 +1,31 @@
 package fa.dfa;
 /**
- *  
+ *  Creates a DFAState object to implement in the DFA diagram itself.
  *  @author Heather N. Larsen, Taylor Roberts
  *  @version    1.0     2018/09/14:13:52
  */
 public class DFAState extends fa.State {
+    /** Determines whether the state is final or not */
+    private boolean finalState;    
+    /***************************GLOBAL.VARIABLES********************************/
     /**
      *  Default constructor.
+     *  @param label name of the state
      */
-    public DFAState() {
-        
+    public DFAState(String label) {
+        super.name      = label;
+        this.finalState = false;
     }/*****************************DFA.STATE************************************/
+    /**
+     *  Sets the state as a final state.
+     */
+    protected void setFinal() {
+        this.finalState = true;
+    }/*****************************SET.FINAL************************************/
+    /**
+     *  @return true if the state is a final state
+     */
+    protected boolean isFinal() {
+        return finalState;
+    }/******************************IS.FINAL************************************/
 }/*******************************DFA.STATE_CLASS********************************/
